362510878f54601e5873cc3e39bdd2bcf69c22dd
sopho-s <woodsnicholas01@gmail.com>
2024-06-07 17:39:49 +0100

    new client id system

--- src/Client.py
+++ src/Client.py
@@ -1,18 +1,17 @@
 import Talk.NetObject.Client
 import random
-import os
+import os.path
 
 
 with open("clientnames.csv", "r") as f:
     names = f.read().split(",")
 name = ""
-key = os.environ.get("CLIENTID", False)
-if key:
-    name = names[int(key)]
+if os.path.isfile("~/clientid.key"):
+    with open("~/clientid.key", "r") as f:
+        name = names[int(f.read())]
 else:
     key = random.randint(0, len(names))
-    os.environ["CLIENTID"] = str(key)
-    with open("clientid.key", "w") as f:
+    with open("~/clientid.key", "w") as f:
         f.write(str(key))
     name = names[key]
 Client = Talk.NetObject.Client.CommandClient(name.encode("utf-8"))
