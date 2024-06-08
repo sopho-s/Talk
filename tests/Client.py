539f529fffe3c3b1bff16d9239c38aa106b31b9a
sopho-s <woodsnicholas01@gmail.com>
2024-06-08 16:49:06 +0100

    work?

--- tests/Client.py
+++ tests/Client.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from src.Talk.NetObject import Client as C
 import random
 import os.path
--- tests/main.py
+++ tests/main.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from src.Talk.GUI import Interface as talkint
 
 talkint.main()
