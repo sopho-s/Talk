a7b0f869b83a57331710bbc02c147897a83d3829
Nick Woods <woodsnicholas01@gmail.com>
2024-06-08 15:49:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "Talk",
+    name = "STalk",
     version = "0.0.1",
     author = "Nicholas woods",
     author_email = "woodsnicholas01@gmail.com",
