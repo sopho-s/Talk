3074ebeffaf55a0dd047bd6ee11b41db5b6f8e1c
sopho-s <woodsnicholas01@gmail.com>
2024-06-09 14:58:39 +0100

    connection fix

--- src/Talk/NetObject/Connection.py
+++ src/Talk/NetObject/Connection.py
@@ -29,7 +29,7 @@ class Connection:
         bytes = bytearray()
         while True:
             data = self.connection.recv(1024)
-            if data[-1] == b" ":
+            if data == b" ":
                 break
             else:
                 bytes.extend(data)
