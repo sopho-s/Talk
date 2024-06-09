e6fe6eb4cc34d3f70b37d90cbfb1f5840106c4ce
Nick Woods <woodsnicholas01@gmail.com>
2024-06-09 15:35:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "STalk",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "Nicholas woods",
     author_email = "woodsnicholas01@gmail.com",
     description = "Allows easy creation of a client server model",
