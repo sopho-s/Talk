3f188f433f1322b40b88838e1170afdc091ddada
sopho-s <woodsnicholas01@gmail.com>
2024-06-10 21:12:53 +0000

    Auto adding checksum

--- src/Talk/NetObject/Client.py
+++ src/Talk/NetObject/Client.py
@@ -29,7 +29,7 @@ class SleepyClient:
 
 class CommandClient:
     def __init__(self, name, commands, id, key=None):
-        self.checksum = 1718052583
+        self.checksum = 1718053973
         self.name = name
         self.workerthread = None
         self.workerobject = None
