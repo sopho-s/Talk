bdadb6a9a864b612684e77c8896c8e0593019195
sopho-s <woodsnicholas01@gmail.com>
2024-06-10 21:46:26 +0100

    fix?

--- src/Talk/NetObject/Server.py
+++ src/Talk/NetObject/Server.py
@@ -36,11 +36,7 @@ class StatusWidgit:
         self.busywidgit.grid(row=4)
 class Server:
     def __init__(self, HOST, PORT, widgit, key=None):
-<<<<<<< HEAD
         self.checksum = 1718052305
-=======
-        self.checksum = 1718052305
->>>>>>> aed796864bf2f1d7fa666503b3d37c722b2e3483
         self.HOST = HOST
         self.PORT = PORT
         self.widgit = widgit
