193ab632c10b304196395df377b6fe8858b700e9
sopho-s <woodsnicholas01@gmail.com>
2024-06-10 23:04:43 +0100

    test

--- src/Talk/NetObject/Client.py
+++ src/Talk/NetObject/Client.py
@@ -29,7 +29,7 @@ class SleepyClient:
 
 class CommandClient:
     def __init__(self, name, commands, id, key=None):
-        self.checksum = 1718056887
+        self.checksum = 1718056882
         self.name = name
         self.workerthread = None
         self.workerobject = None
