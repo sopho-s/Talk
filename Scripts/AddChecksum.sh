0c1737b547ff60ae58767978c3261a425e8b8a01
Nick Woods <woodsnicholas01@gmail.com>
2024-06-10 19:37:04 +0100

    Update AddChecksum.sh

--- Scripts/AddChecksum.sh
+++ Scripts/AddChecksum.sh
@@ -1,5 +1,5 @@
 DATE=$(date +%s)
 sed -i 's/(?<!self\.checksum)0/$DATE/g' src/Talk/NetObject/Server.py
-sed -i 's/(?<!self\.checksum)0/$DATE/g' src/Talk/NetObject/Client.py
+sed -i 's/a/$DATE/g' src/Talk/NetObject/Client.py
 cat src/Talk/NetObject/Server.py
 cat src/Talk/NetObject/Client.py
